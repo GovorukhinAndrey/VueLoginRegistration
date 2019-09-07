f8895d1135c593fb34c31cb33593532b8dbc502c
AndreyGovorukhin <andr.govoruhin@gmail.com>
2019-09-07 21:10:05 +0100

    add vue cli

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+const Login = () => import('@/components/Controller.vue');
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Login,
+  },
+};
+</script>
+
+<style lang="scss">
+/* Import Font Awesome Icons Set */
+$fa-font-path: '~font-awesome/fonts/';
+@import '~font-awesome/scss/font-awesome.scss';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div
+></template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'login',
+  data: () => ({
+    closeController: true,
+    controllerNumber: 0,
+  }),
+  created() {},
+  computed: {},
+  methods: {},
+};
+</script>
