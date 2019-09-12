1351dbc9d1225db27d9699f00c44b7fd3b818e6c
AndreyGovorukhin <andr.govoruhin@gmail.com>
2019-09-12 04:45:57 +0100

    del default component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Login></Login>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-const Login = () => import('@/components/Controller.vue');
+const Login = () => import('@/components/Login.vue');
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
     Login,
   },
 };
