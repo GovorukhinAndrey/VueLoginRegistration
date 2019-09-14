b43c18faec84572014c8365d4ab718ced54e8990
AndreyGovorukhin <andr.govoruhin@gmail.com>
2019-09-14 09:53:20 +0100

    fix: правка стилей

--- src/App.vue
+++ src/App.vue
@@ -24,11 +24,7 @@ export default {
 $fa-font-path: '~font-awesome/fonts/';
 @import '~font-awesome/scss/font-awesome.scss';
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 30px;
   .img-logo {
     max-width: 100px;
