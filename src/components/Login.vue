f8eb78dcce880ff88a7c375e062507ba6b9486fe
AndreyGovorukhin <andr.govoruhin@gmail.com>
2019-09-14 12:31:31 +0100

    вынес в отдельный файл правила для валидаци

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -147,31 +147,8 @@
 <script>
 import axios from '@/axios.js';
 import { ValidationObserver, ValidationProvider } from 'vee-validate';
-import { extend } from 'vee-validate';
 import { validate } from 'vee-validate';
-import { required, email, min, regex } from 'vee-validate/dist/rules';
-
-// Add a rule.
-extend('email', {
-  ...email,
-  message: 'Не корректный E-mail',
-});
-// extend('email', email);
-extend('required', {
-  ...required,
-  message: 'Обязательное поле',
-});
-extend('min', min);
-
-extend('fullName', {
-  ...regex,
-  message: 'Введите Правильное ФИО', // the error message
-});
-
-extend('telephone', {
-  ...min,
-  message: 'введите больше 10 цифр',
-});
+import '@/vue-validate.js';
 
 export default {
   components: {
